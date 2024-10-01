e6e3e53e60c53daa66eca5e151fff80b2d2d8d23
Allaan7 <allaan.salim@gmail.com>
2024-10-01 12:12:37 +0200

    Ajout du router

--- index.php
+++ index.php
@@ -1,8 +1,13 @@
-<?php 
-require_once(__DIR__ . '/partials/head.php');
-?>
+<?php
+require "vendor/autoload.php";
 
-<?php 
-require_once(__DIR__ . '/partials/footer.php');
+use Config\Router;
 
+$router = new Router();
 
+/** j'utilise la methode addRouute de mon controller pour ajouter des routes au controller
+ *  cette methode prends trois argument, la route, le controller et la methode executé
+ */
+$router->addRoute('/', 'HomeController', 'index');
+
+$router->handleRequest();
